65a6dc258fd4e4b6b1a86ca97491686751643f4d
Suffix30 <Suffix30@gmail.com>
2024-11-20 12:46:49 -0500

    Update Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Book-Wurm/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
